ff0a5bed67814d1acb581b990b327aacc8201d8f
Kostya <ckutls228@gmail.com>
2024-04-18 22:17:49 +0300

    added part styles for main-page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,25 @@
 <template>
   <main class="main main-bg">
     <div class="main__content relative z-10 top-40">
-      <h1 class="text-white">121231233123122133</h1>
+      <div class="main-text text-center text-white text-2xl font-rubik font-medium mt-16">
+        <h1 class="font-bold">Форум для оказания помощи связанной с Программированием</h1>
+        <p class="mt-4">Помощь по любому вопросу от наших пользователей</p>
+        <div class="main-text__button mt-12">
+          <NuxtLink to="/" class="bg-orange-200 p-6 rounded-lg hover:text-gray-300 hover:bg-orange-400 default-animation">Перейти на форум</NuxtLink>
+        </div>
+      </div>
     </div>
   </main>
+  <section class="content ml-12 -mt-32">
+    <div class="content__container">
+      <h1>Последние публикации</h1>
+      <div class="publications__content">
+        <div class="publication-element">
+          <h1>123</h1>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup>
