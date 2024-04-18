b431c45b4a4708259a5205c319fe79c7f0929ae2
Kostya <ckutls228@gmail.com>
2024-04-18 21:40:00 +0300

    refactor styles for animation, added to atribute in header-component

--- frontend/components/HeaderComponent.vue
+++ frontend/components/HeaderComponent.vue
@@ -3,13 +3,13 @@
     <div class="header__content flex justify-between m-6 text-white font-roboto font-bold max-sm:flex-col max-sm:text-center">
       <div class="header__logo text-4xl max-sm:text-2xl">
         <NuxtLink to="/">
-          <h1 class="header-animation-text">Helpcode.hub</h1>
+          <h1 class="header-animation-text default-animation">Helpcode.hub</h1>
         </NuxtLink>
       </div>
       <div class="header__nav text-2xl max-sm:mt-4 max-sm:text-lg">
         <nav>
-          <NuxtLink class="mx-4 header-animation-text">О нас</NuxtLink>
-          <NuxtLink class="mx-4 header-animation-text">Форум</NuxtLink>
+          <NuxtLink to="/" class="mx-4 header-animation-text default-animation">О нас</NuxtLink>
+          <NuxtLink to="/" class="mx-4 header-animation-text default-animation">Форум</NuxtLink>
         </nav>
       </div>
     </div>
