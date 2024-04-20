c96ebd060cf64ff95b2fa39e643783b15246281c
Kostya <ckutls228@gmail.com>
2024-04-20 15:22:07 +0300

    added styles for about-page

---
+++ frontend/pages/about.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="about main-bg">
+    <div class="about__content relative z-10 top-60 text-white text-center text-2xl font-semibold font-roboto">
+      <h1>Сайт предназначен для помощи друг друга как сказано на главной странице.</h1>
+      <p class="mb-4">Создан одним человеком, и проект Open-Source</p>
+      <a class="text-blue-400 hover:text-blue-700 default-animation"  href="https://github.com/CkutlsGit/helpcode" target="_blank">Github-проекта</a>
+    </div>
+  </section>
+  <section class="developer">
+    <div class="developer__content -mt-32 text-center font-rubik font-medium">
+      <h1 class="text-2xl">Создатель сайта</h1>
+      <div class="developer-card text-xl my-4 border-2 w-64 h-32 mx-auto">
+        <h2 class="my-2">Константин М.А</h2>
+        <h2 class="my-2">Социальные сети</h2>
+        <div class="developer-card__social text-blue-400">
+          <a class="hover:text-blue-700 default-animation" href="https://github.com/CkutlsGit" target="_blank">Github</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
