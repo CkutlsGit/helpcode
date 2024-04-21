5732dd4c7758d00104c9ded557001d8c0bcc909f
Kostya <ckutls228@gmail.com>
2024-04-21 12:58:14 +0300

    added template for user-topic

--- frontend/layouts/forum.vue
+++ frontend/layouts/forum.vue
@@ -4,7 +4,7 @@
     <div class="forum__container font-rubik border-r-2 border-l-2 border-gray-400 min-h-screen max-h-screen container mx-auto">
       <div class="forum__content max-h-screen overflow-y-auto">
         <div class="forum__content-create text-center p-2 bg-gray-200">
-          <button class="bg-gray-300 rounded-lg py-2 px-12 font-medium text-xl hover:bg-gray-500 default-animation">Создать тему</button>
+          <button v-if="user !== ''" class="bg-gray-300 rounded-lg py-2 px-12 font-medium text-xl hover:bg-gray-500 default-animation">Создать тему</button>
           <h2 class="mt-2" v-if="user === ''">
             <NuxtLink class="hover:text-blue-400 font-bold default-animation" to="/about">Войти</NuxtLink> или <NuxtLink class="hover:text-red-400 font-bold default-animation" to="/">Зарегистрироваться</NuxtLink>
           </h2>
@@ -17,7 +17,7 @@
 </template>
 
 <script setup>
-  const user = ''
+  const user = 'admin'
 </script>
 
 <style scoped>
