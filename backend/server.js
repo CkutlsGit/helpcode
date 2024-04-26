748f561c30976c9290d41267cb289c36472a5876
Kostya <ckutls228@gmail.com>
2024-04-26 15:43:58 +0300

    added api for register user

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import bodyParser from 'body-parser';
 import { db } from './db/index.js'
+import { UserDB } from "./db/dbTypes/user.js";
+import { ForumDB } from "./db/dbTypes/forum.js";
 
 const app = express()
 const port = 3030
+
+const userDB = new UserDB()
+const forumDB = new ForumDB()
 
 app.use(bodyParser.json())
 
@@ -25,7 +30,31 @@ app.get('/', async (req, res) => {
 })
 
 app.post('/api/register', async (req, res) => {
+    const { username, password } = req.body
 
+    if (!username || !password) {
+        return res.status(400).json({ error: 'Ошибка в логине или с паролем' })
+    }
+    if (password.length < 6) {
+        return res.status(400).json({ error: 'Пароль должен быть больше 6 символов' })
+    }
+
+    db.db.get('SELECT * FROM users WHERE username = ?', [username], (err, row) => {
+        if (err) {
+            return res.status(500).json({ error: 'Ошибка при проверке уникальности логина.' })
+        }
+        if (row) {
+            return res.status(400).json({ error: 'Логин должен быть уникальным! Текущий логин занят.' })
+        }
+
+        db.db.run('INSERT INTO users (username, password) VALUES (?, ?)', [username, password], (err) => {
+            if (err) {
+                console.log(err.message)
+                return res.status(500).json({ error: 'Ошибка при регистрации пользователя' })
+            }
+            res.status(201).json({ message: 'Пользователь успешно зарегистрирован.' })
+        })
+    })
 })
 
 app.listen(port, () => {
