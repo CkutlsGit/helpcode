e3bcdaaee999d0c8e9eb71ca6bf5720afe5f43b8
Kostya <ckutls228@gmail.com>
2024-04-30 16:05:50 +0300

    added logic for frontend-server, added request in auth page in frontend-server

--- frontend/pages/forum/user-action/auth.vue
+++ frontend/pages/forum/user-action/auth.vue
@@ -3,9 +3,9 @@
     Вход
   </h1>
   <div class="user-action__inputs flex flex-col gap-y-3 w-64 mx-auto items-center">
-    <input class="border-gray-400 border-2 bg-gray-300 text-white font-bold p-2" type="text" placeholder="Введите логин">
-    <input class="border-gray-400 border-2 bg-gray-300 text-white font-bold p-2" :type="`${ typeInput }`" placeholder="Введите пароль">
-    <button class="mt-6 py-2 px-12 bg-gray-300 rounded-lg text-white text-xl font-bold hover:bg-gray-700 default-animation">
+    <input class="border-gray-400 border-2 bg-gray-300 text-white font-bold p-2" v-model="username" type="text" placeholder="Введите логин">
+    <input class="border-gray-400 border-2 bg-gray-300 text-white font-bold p-2" v-model="password" placeholder="Введите пароль">
+    <button @click="authUser" class="mt-6 py-2 px-12 bg-gray-300 rounded-lg text-white text-xl font-bold hover:bg-gray-700 default-animation">
       Войти
     </button>
   </div>
@@ -15,6 +15,28 @@
   definePageMeta({
     layout: 'user-action'
   })
+
+  const username = ref('')
+  const password = ref('')
+
+  async function authUser() {
+    if (username.value !== '' && password.value !== '') {
+      try {
+        const response = await $fetch('/api/frontend-action/user-action/auth', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ username: username.value, password: password.value })
+        })
+
+        console.log(response)
+      }
+      catch (error) {
+        console.error(error)
+      }
+    }
+  }
 </script>
 
 <style scoped>
---
+++ frontend/server/api/frontend-action/user-action/auth.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default defineEventHandler( async (event) => {
+    const { username, password } = await readBody(event)
+    let responseRequest = 'undefined'
+
+    const result = await axios.post('http://localhost:3030/api/auth', {
+        username: username,
+        password: password
+    })
+        .then(response => {
+            responseRequest = response.data
+        })
+        .catch(error => {
+            console.error(error)
+            responseRequest = error
+        })
+
+    return responseRequest
+})
