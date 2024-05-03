99ff937ce9afaae7f7484991e3ac48d920556b43
Kostya <ckutls228@gmail.com>
2024-05-03 16:18:19 +0300

    added logs for back-server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from 'express'
-import bodyParser from 'body-parser';
+import bodyParser from 'body-parser'
 import { db } from './db/index.js'
-import { UserDB } from "./db/dbTypes/user.js";
-import { ForumDB } from "./db/dbTypes/forum.js";
+import { UserDB } from "./db/dbTypes/user.js"
+import { ForumDB } from "./db/dbTypes/forum.js"
 
 const app = express()
 const port = 3030
@@ -20,9 +20,11 @@ app.get('/', async (req, res) => {
         }
         else {
             if (row) {
+                console.log('Таблица пользователей существует')
                 res.send('Таблица пользователей существует')
             }
             else {
+                console.log('Таблицы пользователей не существует')
                 res.send('Таблицы пользователей не существует')
             }
         }
@@ -33,25 +35,30 @@ app.post('/api/register', async (req, res) => {
     const { username, password } = req.body
 
     if (!username || !password) {
+        console.log('Ошибка в логине или с паролем')
         return res.status(400).json({ error: 'Ошибка в логине или с паролем' })
     }
     if (password.length < 6) {
+        console.log('Пароль должен быть больше 6 символов')
         return res.status(400).json({ error: 'Пароль должен быть больше 6 символов' })
     }
 
     db.db.get('SELECT * FROM users WHERE username = ?', [username], (err, row) => {
         if (err) {
+            console.error('Ошибка при проверке уникальности логина.', err.message)
             return res.status(500).json({ error: 'Ошибка при проверке уникальности логина.' })
         }
         if (row) {
+            console.log('Логин должен быть уникальным! Текущий логин занят.')
             return res.status(400).json({ error: 'Логин должен быть уникальным! Текущий логин занят.' })
         }
 
         db.db.run('INSERT INTO users (username, password) VALUES (?, ?)', [username, password], (err) => {
             if (err) {
-                console.log(err.message)
+                console.error('Ошибка при регистрации пользователя', err.message)
                 return res.status(500).json({ error: 'Ошибка при регистрации пользователя' })
             }
+            console.log('Пользователь успешно зарегистрирован.')
             res.status(201).json({ message: 'Пользователь успешно зарегистрирован.' })
         })
     })
@@ -61,21 +68,26 @@ app.post('/api/auth', (req, res) => {
     const { username, password } = req.body
 
     if (!username || !password) {
+        console.log('Ошибка в логине или с паролем')
         return res.status(400).json({ error: 'Ошибка в логине или с паролем' })
     }
 
     db.db.get('SELECT * FROM users WHERE username = ?', [username], (err, row) => {
         if (err) {
+            console.error('Ошибка при проверке пользователя.', err.message)
             return res.status(500).json({ error: 'Ошибка при проверке пользователя.' })
         }
         if (!row) {
+            console.log('Пользователь не найден.')
             return res.status(404).json({ error: 'Пользователь не найден.' })
         }
 
         if (row.password !== password) {
+            console.log('Неверный пароль.')
             return res.status(401).json({ error: 'Неверный пароль.' })
         }
 
+        console.log('Авторизация успешна.')
         res.status(200).json({ message: 'Авторизация успешна.' })
     })
 })
